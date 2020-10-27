d48185e94fbaf837a58886d79e62a1e4ce5f1526
nas <nas.silver.bullet@gmail.com>
2020-10-28 00:40:08 +0900

    add sub

--- Dockerfile
+++ Dockerfile
@@ -13,8 +13,8 @@ RUN apt-get update \
       && apt-get clean -y \
       && rm -rf /var/lib/apt/lists/*
 
-RUN wget --no-check-certificate https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz \
-      && sudo tar -C /usr/local -xzf go1.11.5.linux-amd64.tar.gz \
-      && rm -rf go1.11.5.linux-amd64.tar.gz
+RUN wget --no-check-certificate https://dl.google.com/go/go1.15.3.linux-amd64.tar.gz \
+      && sudo tar -C /usr/local -xzf go1.15.3.linux-amd64.tar.gz \
+      && rm -rf go1.15.3.linux-amd64.tar.gz
 
 ENV PATH $PATH:/usr/local/go/bin
--- main.go
+++ main.go
@@ -4,8 +4,23 @@ import (
 	"errors"
 	"fmt"
 	"os"
-	"strconv"
 )
+
+const (
+	numberOfArgs = 2
+)
+
+var (
+	ErrIncorrectNumberArgument = errors.New("the number of arguments is not correct")
+)
+
+type ErrUnexpectedChar struct {
+	Message string
+}
+
+func (e ErrUnexpectedChar) Error() string {
+	return fmt.Sprintf("unexpected char, %s", e.Message)
+}
 
 func main() {
 	if err := run(); err != nil {
@@ -17,20 +32,81 @@ func main() {
 }
 
 func run() error {
-	if len(os.Args) != 2 {
-		return errors.New("引数の個数が正しくありません")
+	if len(os.Args) != numberOfArgs {
+		return ErrIncorrectNumberArgument
 	}
 
-	i, err := strconv.Atoi(os.Args[1])
-	if err != nil {
-		return errors.New("引数の個数が正しくありません")
-	}
+	p := os.Args[1]
 
 	fmt.Println(".intel_syntax noprefix")
 	fmt.Println(".globl main")
 	fmt.Println("main:")
+
+	i, n := strToInt(p)
+
+	if n == 0 {
+		return ErrUnexpectedChar{Message: p}
+	}
+
 	fmt.Printf("  mov rax, %d\n", i)
+
+	for i := n; i < len(p); i++ {
+		if p[i] == '+' {
+			i++
+
+			operand, digits := strToInt(p[i:])
+
+			if digits == 0 {
+				return ErrUnexpectedChar{Message: p}
+			}
+
+			fmt.Printf("  add rax, %d\n", operand)
+
+			i += digits - 1
+
+			continue
+		}
+
+		if p[i] == '-' {
+			i++
+
+			operand, digits := strToInt(p[i:])
+
+			if digits == 0 {
+				return ErrUnexpectedChar{Message: p}
+			}
+
+			fmt.Printf("  sub rax, %d\n", operand)
+
+			i += digits - 1
+
+			continue
+		}
+
+		return ErrUnexpectedChar{Message: p}
+	}
+
 	fmt.Println("  ret")
 
 	return nil
+}
+
+func strToInt(s string) (int, int) {
+	var (
+		n         int
+		digits    int = 1
+		increment int
+	)
+
+	for i, c := range s {
+		if c < '0' || '9' < c {
+			break
+		}
+
+		n = n*digits + int(c-'0')
+		digits *= 10
+		increment = i + 1
+	}
+
+	return n, increment
 }
